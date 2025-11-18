f979436f096c0ec6c67fcd1bd464beb0a7cb4bf9
Abhiram Anand <abhiramanand25@gmail.com>
2025-11-18 10:16:39 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py - SAFE VERSION
-import os
-
-# Get API key from environment variables
-GROQ_API_KEY = os.getenv("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")
-
-# Alternative: For Streamlit apps
-# import streamlit as st
-# GROQ_API_KEY = st.secrets.get("GROQ_API_KEY")
